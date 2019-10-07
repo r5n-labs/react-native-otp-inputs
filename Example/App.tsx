c5bc824d05c0bb572de070c0e817207de80b83e7
Damian Sznajder <damsznajder@gmail.com>
2019-10-07 21:28:34 +0200

    Fix example for notches

--- Example/App.tsx
+++ Example/App.tsx
@@ -1,5 +1,5 @@
 import React, { RefObject, Component } from 'react';
-import { StyleSheet, Button, View } from 'react-native';
+import { SafeAreaView, StyleSheet, Button, View } from 'react-native';
 
 // @ts-ignore
 import OtpInputs from 'react-native-otp-inputs';
@@ -13,10 +13,10 @@ export default class App extends Component {
 
   render() {
     return (
-      <View style={styles.container}>
+      <SafeAreaView style={styles.container}>
         <Button title="Reset" onPress={this.resetOTP} />
         <OtpInputs ref={this.otpRef} numberOfInputs={4} />
-      </View>
+      </SafeAreaView>
     );
   }
 }

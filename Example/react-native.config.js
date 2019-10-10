a28377e60295a22ce9073a5a73f694dff60e2589
Michal Kantor <kantorm96@gmail.com>
2019-10-10 10:58:27 +0200

    Configure reac-native-keyevent in Example project

---
+++ Example/react-native.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+  dependencies: {
+    'react-native-keyevent': {
+      platforms: {
+        ios: null, // disable iOS platform, other platforms will still autolink if provided
+      },
+    },
+  },
+};

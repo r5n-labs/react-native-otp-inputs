4eb5d7fb380387e9e80baa98f173eb176084a273
Michal Kantor <kantorm96@gmail.com>
2020-01-28 11:20:00 +0100

    Remove react-native-keyevent and check for keypress support

--- Example/android/app/src/main/java/com/example/MainActivity.java
+++ Example/android/app/src/main/java/com/example/MainActivity.java
@@ -2,9 +2,6 @@ package com.example;
 
 import com.facebook.react.ReactActivity;
 
-import android.view.KeyEvent; // <--- import
-import com.github.kevinejohn.keyevent.KeyEventModule; // <--- import
-
 public class MainActivity extends ReactActivity {
 
     /**
@@ -15,58 +12,4 @@ public class MainActivity extends ReactActivity {
     protected String getMainComponentName() {
         return "Example";
     }
-
-    @Override  // <--- Add this method if you want to react to keyDown
-    public boolean onKeyDown(int keyCode, KeyEvent event) {
-
-        // A. Prevent multiple events on long button press
-        //    In the default behavior multiple events are fired if a button
-        //    is pressed for a while. You can prevent this behavior if you
-        //    forward only the first event:
-        //        if (event.getRepeatCount() == 0) {
-        //            KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
-        //        }
-        //
-        // B. If multiple Events shall be fired when the button is pressed
-        //    for a while use this code:
-        //        KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
-        //
-        // Using B.
-        KeyEventModule.getInstance().onKeyDownEvent(keyCode, event);
-
-        // There are 2 ways this can be done:
-        //  1.  Override the default keyboard event behavior
-        //    super.onKeyDown(keyCode, event);
-        //    return true;
-
-        //  2.  Keep default keyboard event behavior
-        //    return super.onKeyDown(keyCode, event);
-
-        // Using method #1 without blocking multiple
-        super.onKeyDown(keyCode, event);
-        return true;
-      }
-
-    @Override  // <--- Add this method if you want to react to keyUp
-    public boolean onKeyUp(int keyCode, KeyEvent event) {
-        KeyEventModule.getInstance().onKeyUpEvent(keyCode, event);
-
-        // There are 2 ways this can be done:
-        //  1.  Override the default keyboard event behavior
-        //    super.onKeyUp(keyCode, event);
-        //    return true;
-
-        //  2.  Keep default keyboard event behavior
-        //    return super.onKeyUp(keyCode, event);
-
-        // Using method #1
-        super.onKeyUp(keyCode, event);
-        return true;
-      }
-
-    @Override
-    public boolean onKeyMultiple(int keyCode, int repeatCount, KeyEvent event) {
-          KeyEventModule.getInstance().onKeyMultipleEvent(keyCode, repeatCount, event);
-          return super.onKeyMultiple(keyCode, repeatCount, event);
-      }
 }
--- Example/react-native.config.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  dependencies: {
-    'react-native-keyevent': {
-      platforms: {
-        ios: null,
-      },
-    },
-  },
-};

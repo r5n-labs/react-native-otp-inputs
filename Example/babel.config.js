1a09bbbfb44f160586f829afd32d63b250b599ff
Damian Sznajder <damsznajder@gmail.com>
2021-03-31 17:01:17 +0200

    chore: update packages & use newest bob builder structure

--- Example/babel.config.js
+++ Example/babel.config.js
@@ -1,13 +1,20 @@
+const pak = require('../package.json');
+
+const path = require('path');
+
 module.exports = function (api) {
   api.cache(true);
+
   return {
-    presets: ['expo'],
+    presets: ['babel-preset-expo'],
     plugins: [
       [
         'module-resolver',
         {
+          extensions: ['.tsx', '.ts', '.js', '.json'],
           alias: {
-            'react-native-otp-inputs': '../src/index',
+            // For development, we want to alias the library to the source
+            [pak.name]: path.join(__dirname, '..', pak.source),
           },
         },
       ],
